c7b5a9a0e5cd6490099ebe069cf412787603bee0
DrCharlesS <drcharlesshi@gmail.com>
2024-10-26 01:44:42 -0700

    Added player controls

---
+++ Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,65 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour
+{
+    public float walkSpeed = 5f;
+    public float runSpeed = 10f;
+    public float jumpHeight = 1.5f;
+    public float mouseSensitivity = 100f;
+    public Camera playerCamera;
+
+    private CharacterController characterController;
+    private float yRotation = 0f;
+    private float gravity = -9.81f;
+    private Vector3 velocity;
+
+    void Start()
+    {
+        characterController = GetComponent<CharacterController>();
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    void Update()
+    {
+        HandleMovement();
+        HandleMouseLook();
+    }
+
+    void HandleMovement()
+    {
+        float speed = Input.GetKey(KeyCode.LeftShift) ? runSpeed : walkSpeed;
+
+        float moveX = Input.GetAxis("Horizontal");
+        float moveZ = Input.GetAxis("Vertical");
+
+        Vector3 move = transform.right * moveX + transform.forward * moveZ;
+        characterController.Move(move * speed * Time.deltaTime);
+
+        if (characterController.isGrounded && velocity.y < 0)
+        {
+            velocity.y = -2f;
+        }
+
+        if (Input.GetButtonDown("Jump") && characterController.isGrounded)
+        {
+            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+        }
+
+        velocity.y += gravity * Time.deltaTime;
+        characterController.Move(velocity * Time.deltaTime);
+    }
+
+    void HandleMouseLook()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+
+        yRotation -= mouseY;
+        yRotation = Mathf.Clamp(yRotation, -90f, 90f);
+
+        playerCamera.transform.localRotation = Quaternion.Euler(yRotation, 0f, 0f);
+        transform.Rotate(Vector3.up * mouseX);
+    }
+}
